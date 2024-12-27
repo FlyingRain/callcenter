582a3629e2528afec941eb76203991ed50bc93f6
wally <wally@flyingrain.com>
2024-12-27 20:47:15 +0800

    add changes

--- test_call/src/components/callwindow.vue
+++ test_call/src/components/callwindow.vue
@@ -1,11 +1,11 @@
 <template>
 
   <div class="callwindow-container">
-
+    <calltip ref="tip" :audioRef="audio"></calltip>
     <div ref="draggable" class="draggable" @mousedown="dragStart" @touchstart="dragStart">
       <el-row :gutter="12">
         <el-col :span="4">
-          <span>阿森松</span>
+          <span>阿波罗</span>
         </el-col>
         <el-col :span="4">
           <span style="color: green"> 在线</span>
@@ -21,13 +21,53 @@
         </el-col>
       </el-row>
     </div>
+    <div class="animated-text">韭菜乐</div>
+
+    <div style="display: flex;justify-content:center;min-width: 100vh;min-height: 100vh;">
+      <div style="width: 30%">
+        <p>外显号码设置</p>
+        <el-input size="default" placeholder="设置要显示的号码" style="width: 200px" v-model="callSettings.showNumber"
+                  @blur="changeSetting"/>
+      </div>
+      <div style="border:  2px solid black;min-height: 100vh;margin: 10px">
+      </div>
+      <div style="height: 100%;width: 30%">
+        <p>呼入转接配置</p>
+        <el-button size="small" type="primary" @click="addCallInMap">增加</el-button>
+        <div v-for="(item,index) in callSettings.callInMap" style="margin-top: 10px;">
+          <div style="display: flex">
+            <span>呼入号码：</span>
+            <el-input size="default" v-model="item.callInNumber" style="width: 200px"
+                      placeholder="呼入号码"></el-input>
+          </div>
+          <div style="display: flex">
+            <span>匹配坐席：</span>
+            <el-select v-model="item.agent" style="width: 200px" placeholder="选择坐席">
+              <el-option label="普罗米修斯" key="1" value="1003"/>
+              <el-option label="阿波罗" key="2" value="1001"/>
+            </el-select>
+            <el-button @click="deleteCallInMap(item)">删除</el-button>
+          </div>
+        </div>
+        <div style="display: flex;justify-content:end;width: 100%;">
+          <el-button type="primary" @click="changeSetting">应用</el-button>
+        </div>
+      </div>
+      <div style="border:  2px solid black;min-height: 100vh;margin: 10px">
+      </div>
+      <div style="min-height: 100vh;width: 30%;">
+        <p>转接</p>
+        <div style="display: flex">
+          <el-input style="width: 200px;" placeholder="转接号" v-model="transferNumber" size="small"></el-input>
+          <el-button type="primary" @click="transferCall">转接</el-button>
+        </div>
+      </div>
+    </div>
+
     <div>
       <audio ref="audio"></audio>
     </div>
-    <div>
-      <el-button type="primary" @click="callTips">弹窗</el-button>
-    </div>
-    <calltip ref="tip" :audioRef="audio"></calltip>
+
   </div>
 
 
@@ -45,7 +85,10 @@ const draggable = ref()
 const {dragStart} = useDrag(draggable)
 
 var socket = new JsSIP.WebSocketInterface('ws:/192.168.100.197:5066');
+// var socket = new JsSIP.WebSocketInterface('ws:/111.231.64.229:5896');
+//5896
 let uri = new URI("sip", '1003', '192.168.100.197', 5066);
+// let uri = new URI("sip", '1003', '111.231.64.229', 5896);
 let tip = ref()
 
 var configuration = {
@@ -59,9 +102,13 @@ uri.setParam('transport', 'ws')
 configuration.contact_uri = uri.toString()
 let audio = ref()
 
-const {connectFS, makeCall, hangup} = useCall()
-connectFS(configuration, (callInNumber: string) => {
-  tip.value.tips_pop(callInNumber, "up")
+const {connectFS, makeCall, hangup, accept} = useCall()
+connectFS(configuration, (callInNumber: string, autoReceive: boolean) => {
+  if (autoReceive) {
+    accept(audio.value)
+  } else {
+    tip.value.tips_pop(callInNumber, "up")
+  }
 })
 let number = ref('')
 
@@ -77,6 +124,29 @@ function call(number: string) {
 }
 
 // let callSession = makeCall('sip:1002@192.168.0.116')
+
+
+let callSettings = ref({showNumber: '', callInMap: [{callInNumber: '', agent: ''}]})
+
+function changeSetting() {
+
+}
+
+function addCallInMap() {
+  callSettings.value.callInMap.push({callInNumber: '', agent: ''})
+}
+
+function deleteCallInMap(item: any) {
+  let index = callSettings.value.callInMap.indexOf(item)
+  callSettings.value.callInMap.splice(index, 1);
+}
+
+let transferNumber = ref('')
+
+function transferCall(){
+
+}
+
 </script>
 
 
@@ -93,5 +163,36 @@ function call(number: string) {
   width: 400px;
   cursor: move;
   background-color: skyblue;
+}
+
+
+/* 定义动画 */
+@keyframes colorFade {
+  0% {
+    background-position: left;
+    color: #ff2424; /* 开始时字体颜色透明 */
+  }
+  30% {
+    background-position: left;
+    color: #7df816; /* 中间时的目标颜色 */
+  }
+  60% {
+    background-position: right;
+    color: #fd0f87; /* 中间时的目标颜色 */
+  }
+  100% {
+    background-position: right;
+    color: #53ff29; /* 结束时字体颜色透明 */
+  }
+}
+
+/* 应用动画到元素 */
+.animated-text {
+  animation: colorFade 5s infinite; /* 动画名称，持续时间，和重复次数 */
+  font-size: 45px;
+  font-weight: bold;
+  min-width: 100vh;
+  text-align: center;
+  height: 100px;
 }
 </style>
